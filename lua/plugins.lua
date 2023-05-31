b43e52a2e48dd9ffbafdf1811b50569933c61089
hongtingxia <djywithhjh@gmail.com>
2023-05-31 19:48:27 +0800

    1.treesitter 代码高亮

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -37,6 +37,14 @@ return require('packer').startup({
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use {
+			'nvim-treesitter/nvim-treesitter',
+			run = function()
+				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+				ts_update()
+			end,}
+	  use("nvim-treesitter/nvim-treesitter-context")
+
 
 
   
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -89,6 +89,16 @@ _G.packer_plugins = {
     path = "/Users/hongtingxia/.local/share/nvim/site/pack/packer/start/gruvbox-material",
     url = "https://github.com/sainnhe/gruvbox-material"
   },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/Users/hongtingxia/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
+  ["nvim-treesitter-context"] = {
+    loaded = true,
+    path = "/Users/hongtingxia/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/Users/hongtingxia/.local/share/nvim/site/pack/packer/start/packer.nvim",
