35ad3a75779b81c5852b0c4c5a3489c714c607bc
hongtingxia <djywithhjh@gmail.com>
2023-05-31 20:22:38 +0800

    1.add lsp plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,12 +13,12 @@ return require('packer').startup({
 	function()
 		use 'wbthomason/packer.nvim'-- Packer can manage itself
 		use {
-        "vim-airline/vim-airline",
-        requires = {
-          "vim-airline/vim-airline-themes",
-          --综合图标支持such vim-airline lightline, vim-startify
-          "ryanoasis/vim-devicons"
-        	}
+			"vim-airline/vim-airline",
+			requires = {
+				"vim-airline/vim-airline-themes",
+				--综合图标支持such vim-airline lightline, vim-startify
+				"ryanoasis/vim-devicons"
+			}
 		}
 		--内嵌浮动ranger插件
 		use "kevinhwang91/rnvimr"
@@ -32,37 +32,58 @@ return require('packer').startup({
 		}
 		--主题
 		use "sainnhe/gruvbox-material"
-		  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {
+		use {
+			'nvim-telescope/telescope.nvim', tag = '0.1.0',
+			-- or                            , branch = '0.1.x',
+			requires = { {'nvim-lua/plenary.nvim'} }
+		}
+		use {
 			'nvim-treesitter/nvim-treesitter',
 			run = function()
 				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
 				ts_update()
 			end,}
-	  use("nvim-treesitter/nvim-treesitter-context")
-	use("mbbill/undotree")
+			use("nvim-treesitter/nvim-treesitter-context")
+			use("mbbill/undotree")
+			use {
+				'VonHeikemen/lsp-zero.nvim',
+				branch = 'v1.x',
+				requires = {
+					-- LSP Support
+					{'neovim/nvim-lspconfig'},
+					{'williamboman/mason.nvim'},
+					{'williamboman/mason-lspconfig.nvim'},
+
+					-- Autocompletion
+					{'hrsh7th/nvim-cmp'},
+					{'hrsh7th/cmp-buffer'},
+					{'hrsh7th/cmp-path'},
+					{'saadparwaiz1/cmp_luasnip'},
+					{'hrsh7th/cmp-nvim-lsp'},
+					{'hrsh7th/cmp-nvim-lua'},
+
+					-- Snippets
+					{'L3MON4D3/LuaSnip'},
+					{'rafamadriz/friendly-snippets'},
+				}
+			}
 
 
 
 
-  
-	end,
-	config = {
-		max_jobs = 16,
-		git = {
-			--default_url_format = 'https://hub.fastgit.org/%s'
-			--csdn速度稳定，但是很多冷门的项目没有
-			--default_url_format = 'https://codechina.csdn.net/mirrors/%s'
-		},
-		display = {
-			open_fn = function()
-				return require('packer.util').float({ border = 'single' })
-			end
+		end,
+		config = {
+			max_jobs = 16,
+			git = {
+				--default_url_format = 'https://hub.fastgit.org/%s'
+				--csdn速度稳定，但是很多冷门的项目没有
+				--default_url_format = 'https://codechina.csdn.net/mirrors/%s'
+			},
+			display = {
+				open_fn = function()
+					return require('packer.util').float({ border = 'single' })
+				end
+			}
 		}
-	}
-})
+	})
 
