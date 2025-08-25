3b0cc253f3ef5fbce6a90a5487f892e182408a2c
hongtingxia <djywithhjh@gmail.com>
2025-08-25 12:07:52 +0800

    1.plugins修改

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -76,6 +76,18 @@ return require('packer').startup({
 					require("nvim-tree").setup {}
 				end
 			}
+			use {
+  "folke/which-key.nvim",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
 
 
 
